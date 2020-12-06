81170fcb99bb8ca185aaf09e4d5468a3ac64e453
zFaka <fakakill2000@gmail.com>
2020-12-06 17:58:44 -0300

    handleAdd

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['Creeper', 'Enderman', 'Wolf']);
+
+  const handleAdd = () => {
+    setCategories([...categories, 'Slime']);
+  };
   return (
-      <>
-        <h2>GifExpertApp</h2> 
-        <hr/>
-      </>
+    <>
+      <h2>GifExpertApp</h2> 
+      <hr/>
+      <button onClick={handleAdd}>Add</button>
+      <ol>
+        {
+
+          categories.map(category => {
+
+            return <li key={category}> {category} </li>
+
+          })
+
+        }
+      </ol>
+    </>
   )}
 
 export default GifExpertApp ;
